d89b2f093bc0c1279136b92327064c0857c96e66
Andre Nicodemus <andre.nicodemus@gmail.com>
2019-01-21 10:12:18 +0100

    changes on file names

--- js/mobile-payment.js
+++ js/mobile-payment.js
@@ -2,8 +2,13 @@ jQuery(document).ready(function($) {
   setTimeout(function() {
     $('.circle-loader').toggleClass('load-complete');
     $('.checkmark').toggle();
+    $('.error-x').toggle();
+    $('.btn-retry').toggle();
     $('.processing-in-progress').addClass('processing-done');
-    $('.processing-successful').addClass('processing-successful-done');
+    $('.processing-successful').addClass('processing-done');
+    $('#amount-error').hide();
+    $('.processing-text-error').attr('style','opacity:0');
     $('.processing-text').addClass('fade-out');
+    $('.circle-loader-error').toggleClass('load-complete-error');
   }, 3000);
 });
